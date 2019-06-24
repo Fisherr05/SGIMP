800b8d291f3afe4fb46489a7f9ab46663d215a12
DJFiBePRO <carlosfiallos@hotmail.es>
2019-06-24 15:27:22 -0500

    Create drops.sql

---
+++ bd/drops.sql
@@ -0,0 +1,18 @@
+drop table OBJETIVOS_ESPECIFICOS;
+drop table ACTIVIDAD_DOCENTE;
+drop table ACTIVIDAD_ESTUDIANTE;
+drop table ACTIVIDAD;
+drop table COMPONENTE;
+drop table SUBPROYECTO;
+drop table PROYECTO;
+drop table ESTADO;
+drop table REPRESENTANTE_EMPRESA;
+drop table EMPRESA;
+drop table ESTUDIANTE;
+drop table DOCENTE;
+drop table COORDINADOR_CARRERA;
+drop table COORDINADOR_FACULTAD;
+drop table CARRERA;
+drop table FACULTAD;
+drop table COORDINADOR_GENERAL;
+drop table PERSONA ;
