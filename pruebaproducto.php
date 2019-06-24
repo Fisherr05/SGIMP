cdb787695cb302403691a851dbe62b46ede346f0
LuisBuenanio <luisbuenao51@hotmail.com>
2019-06-24 13:58:59 -0500

    Hola

--- pruebaproducto.php
+++ pruebaproducto.php
@@ -1,4 +1,4 @@
-
+﻿
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -34,6 +34,7 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+<!--dsdsfs-->
 
     </head>
 	<body>
